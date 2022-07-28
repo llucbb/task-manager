700801613aa089ee1c5889dd0429d4d47a67b242
Lucas Barbat <llucbb.mac@gmail.com>
2022-07-28 21:47:13 +0200

    Use lombok @Slf4j annotation instead of LogFactory

--- src/main/java/com/celonis/challenge/controllers/ErrorController.java
+++ src/main/java/com/celonis/challenge/controllers/ErrorController.java
@@ -2,36 +2,34 @@ package com.celonis.challenge.controllers;
 
 import com.celonis.challenge.exceptions.NotAuthorizedException;
 import com.celonis.challenge.exceptions.NotFoundException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class ErrorController {
-
-  private final Logger logger = LoggerFactory.getLogger(getClass());
 
   @ResponseStatus(HttpStatus.NOT_FOUND)
   @ExceptionHandler(NotFoundException.class)
   public String handleNotFound() {
-    logger.warn("Entity not found");
+    log.warn("Entity not found");
     return "Not found";
   }
 
   @ResponseStatus(HttpStatus.UNAUTHORIZED)
   @ExceptionHandler(NotAuthorizedException.class)
   public String handleNotAuthorized() {
-    logger.warn("Not authorized");
+    log.warn("Not authorized");
     return "Not authorized";
   }
 
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   @ExceptionHandler(Exception.class)
   public String handleInternalError(Exception e) {
-    logger.error("Unhandled Exception in Controller", e);
+    log.error("Unhandled Exception in Controller", e);
     return "Internal error";
   }
 }
