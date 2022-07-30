ec74ed18ea14588d09f6affbfa5e1b87d588fc03
Lucas Barbat <llucbb.mac@gmail.com>
2022-07-30 17:13:38 +0200

    Testing and documenting

--- src/test/java/com/celonis/challenge/ChallengeApplicationTest.java
+++ src/test/java/com/celonis/challenge/ChallengeApplicationTest.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class ApplicationTest {
+public class ChallengeApplicationTest {
 
   @Autowired private TaskController taskController;
 
