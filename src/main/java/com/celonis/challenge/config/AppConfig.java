dce784bae79eee2260a4a37c7c566dab7f330774
Lucas Barbat <llucbb.mac@gmail.com>
2022-07-30 18:39:06 +0200

    Testing and documenting

--- src/main/java/com/celonis/challenge/config/AppConfig.java
+++ src/main/java/com/celonis/challenge/config/AppConfig.java
@@ -1,7 +1,6 @@
 package com.celonis.challenge.config;
 
 import com.celonis.challenge.factory.TaskExecutorServiceFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ServiceLocatorFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,9 +10,6 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 public class AppConfig {
 
   private static final int CORES = Runtime.getRuntime().availableProcessors();
-
-  @Value("${application.counter-delay}")
-  private int counterDelay;
 
   @Bean
   public ServiceLocatorFactoryBean taskExecutorServiceFactory() {
@@ -25,11 +21,8 @@ public class AppConfig {
   @Bean
   public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
     ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-    // The following values depend on many factors like the computer running the application, the number and rate of tasks executed, the delay applied to the counter
-    // tasks, etc.
-    // I've decided to use the number of cores of the computer and a pool size big enough to be able
-    // to queue some tasks
     executor.setCorePoolSize(CORES);
+    executor.setMaxPoolSize(CORES);
     executor.setThreadNamePrefix(AppConstants.COUNTER_TASK);
     executor.initialize();
     return executor;
